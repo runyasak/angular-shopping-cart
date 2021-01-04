e76e862b55dd1011b9997371003b6416418e7232
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-04 15:26:29 +0700

    feat: implement product

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from 'src/app/products';
+import type { Product } from 'src/interfaces/Product.interface';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product?: Product;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
 
   ngOnInit(): void {
+    const productIdFromRoute = this.route.snapshot.paramMap.get('productId');
+    this.product = products.find(product => product.id === Number(productIdFromRoute));
   }
 
 }
