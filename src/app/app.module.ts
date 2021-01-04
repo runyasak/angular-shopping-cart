69d1be60ce2ed59b5105020c54919f399e71fce2
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-04 13:46:35 +0700

    feat: add product-alerts and implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
@@ -12,6 +13,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     AppComponent,
     TopBarComponent,
     ProductListComponent,
+    ProductAlertsComponent,
   ],
   imports: [
     BrowserModule,
