35caa93daf38592c710bc3546547338d715ebb1f
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-05 23:55:34 +0700

    feat: implement form and add Customer interface

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import type { FormGroup } from '@angular/forms';
 import { CartService } from 'src/app/cart.service';
+import type { Customer } from 'src/interfaces/Customer.interface';
 import type { Product } from 'src/interfaces/Product.interface';
 
 @Component({
@@ -9,13 +12,28 @@ import type { Product } from 'src/interfaces/Product.interface';
 })
 export class CartComponent implements OnInit {
   items: Product[] = [];
+  checkoutForm?: FormGroup;
 
   constructor(
     private cartService: CartService,
-  ) { }
+    private formBuilder: FormBuilder,
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: '',
+    });
+  }
 
   ngOnInit(): void {
     this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData: Customer): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm?.reset();
+
+    console.warn('Your order has been submitted', customerData);
   }
 
 }
