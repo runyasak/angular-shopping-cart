0c019c75835a2863e33e72988b76df1603c73ae2
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-05 23:00:05 +0700

    feat: add cart component with route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent,
+    CartComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    ProductListComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'cart', component: CartComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
     ]),
   ],
